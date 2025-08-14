6a091536d5ed6edbdf7157e15747e44617e091c6
Higor S. <stanleyhigor1@gmail.com>
2025-08-14 10:03:40 -0300

    desafios adicionado

--- flashcard/views.py
+++ flashcard/views.py
@@ -162,3 +162,22 @@ def css_dificuldade(self):
         return 'flashcard-medio'
     elif self.dificuldade == 'D':
         return 'flashcard-dificil'
+    
+def relatorio(request, id):
+    desafio = Desafio.objects.get(id=id)
+
+
+    acertos = desafio.flashcards.filter(acertou=True).count()
+    erros = desafio.flashcards.filter(acertou=False).count()
+
+    dados = [acertos, erros]
+
+
+    categorias = desafio.categoria.all()
+    name_categoria = [i.nome for i in categorias]
+
+    dados2 = []
+    for categoria in categorias:
+        dados2.append(desafio.flashcards.filter(flashcard__categoria=categoria).filter(acertou=True).count())
+
+    return render(request, 'relatorio.html', {'desafio': desafio, 'dados': dados, 'categorias': name_categoria, 'dados2': dados2,},)
