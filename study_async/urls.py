e27e108f8ffbf7d9e58ca824cadabe6a8e763732
Higor S. <stanleyhigor1@gmail.com>
2025-08-18 10:45:59 -0300

    adicionado tela de apostilas

--- study_async/urls.py
+++ study_async/urls.py
@@ -18,11 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
 from usuarios.views import home
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
     path('usuarios/', include('usuarios.urls')),
     path('flashcard/', include('flashcard.urls')),
-]
+    path('apostilas/', include('apostilas.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
